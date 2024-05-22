9e63d7925fed854460cbf5a847e9dfb64adaeb29
stepholo <oloobrian89@gmail.com>
2024-05-22 22:32:36 +0300

    Day 2 Challenge

---
+++ day-2100-days/fix.py
@@ -0,0 +1,69 @@
+"""
+my variable = input("WHO GOES THERE? ")
+print(my variable)
+"""
+
+# Corrected Code
+my_variable = input("WHO GOES THERE? ")
+print(my_variable)
+
+"""
+myGrandma = input("How's your Grandma doing? 😘 ")
+print(mygrandma)
+"""
+
+# Corrected Code
+my_grandma = input("How's your Grandma doing? 😘 ")
+print(my_grandma)
+
+"""
+myLunch = input("What are you having for lunch? ")
+print("Hmm, it sounds like you really should just order")
+print("myLunch")
+print("as soon as possible!")
+"""
+
+# Corrected Code
+my_lunch = input("What are you having for lunch? ")
+print("Hmm, it sounds like you really should just order")
+print(my_lunch)
+print("as soon as possible!")
+
+"""
+print("Definitely not a Phishing Scam")
+print()
+input("Your Name")
+print("Thanks for logging in")
+print(myName)
+cardNumber = input("What is your credit card number?")
+print("Thanks, I definitely wont put")
+print("cardNumber")
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maidenName input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print(maidenName)
+print("was your Mum's maiden name. Not because the bank requested it
+        or anything, honest.")
+"""
+
+# Corrected Code
+print("Definitely not a Phishing Scam")
+print()
+my_name = input("Your Name: ")
+print("Thanks for logging in")
+print(my_name)
+card_number = input("What is your credit card number? ")
+print("Thanks, I definitely wont put")
+print(card_number)
+print("into Amazon and order anything weird")
+print()
+print("Promise")
+maiden_name = input("What is your Mother's maiden name? ")
+print()
+print("That's cool, I just wanted to know that")
+print(maiden_name)
+print("was your Mum's maiden name. Not because the bank requested it"
+      "or anything, honest.")
