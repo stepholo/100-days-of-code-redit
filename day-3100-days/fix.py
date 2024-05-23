4b225817bdfa41bef1d3dceef68878efa281ae99
stepholo <oloobrian89@gmail.com>
2024-05-23 22:27:12 +0300

    Day 3 Challenge

---
+++ day-3100-days/fix.py
@@ -0,0 +1,49 @@
+"""
+yourName = input("Name: ")
+whatYear = input("What year is it?: ")
+print(yourName "thinks it is" whatYear)
+"""
+
+# Fix My Code
+yourName = input("Name: ")
+whatYear = input("What year is it?: ")
+print(yourName, "thinks it is", whatYear)
+
+"""
+yourName = input("Name: ")
+whatYear = input("What year is it?: ")
+print("yourName, thinks it is, whatYear")
+"""
+
+# Fix My Code
+yourName = input("Name: ")
+whatYear = input("What year is it?: ")
+print(yourName, "thinks it is", whatYear)
+
+"""
+print("=== Your Song Generator ===")
+print("You'll be asked a bunch of questions
+then we'll make you up an amazing song,
+totally copyright free 😭")
+print()
+person = input("Name a person famous for something good: ")
+thing = input("Name a thing they did: ")
+place = input("Name a place you like: ")
+rhyme = input("Give me a verb that rhymes with your person's name: ")
+print()
+print("There was a person called" name)
+print("Who did something cool like", thing, "at the wonderful", place "where you'll find me", rhyme)
+"""
+
+# Fix My Code
+print("=== Your Song Generator ===")
+print("""You'll be asked a bunch of questions
+then we'll make you up an amazing song,
+totally copyright free 😭""")
+print()
+person = input("Name a person famous for something good: ")
+thing = input("Name a thing they did: ")
+place = input("Name a place you like: ")
+rhyme = input("Give me a verb that rhymes with your person's name: ")
+print()
+print("There was a person called", person, "Who did something cool like", thing, "at the wonderful", place, "where you'll find me", rhyme)
---
+++ day-3100-days/main.py
@@ -0,0 +1,9 @@
+food = input("Name a food: ")
+plant = input("Name a plant: ")
+cooking = input("Name a method of cooking: ")
+burned = input("What word describes burned food: ")
+item = input("Name a DIY item: ")
+
+print()
+print("MENU")
+print(cooking, food, "with", burned, plant, "on a bed of", item)
