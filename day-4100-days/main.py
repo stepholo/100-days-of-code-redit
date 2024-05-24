faa6a1a4dfdba640c483dd20d1b6c30cbfe2a446
stepholo <oloobrian89@gmail.com>
2024-05-24 16:12:40 +0300

    Day 4 Challenge

---
+++ day-4100-days/main.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+enemy = input("What is your worst enemy’s name? ")
+superpower = input("What is your superpower? ")
+live = input("Where do you live? ")
+favFood = input("What is your favorite food? ")
+
+print("Hello", name, "! Your ability to", superpower, "will make sure you never have to look at, ", enemy, "again. Go eat", favFood, "as you walk down the streets of", live, "and use", superpower, "for good and not evil!")
