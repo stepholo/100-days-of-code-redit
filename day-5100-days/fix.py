c63b3442ab2cb040f5f1e4094ff0bc0018929609
stepholo <oloobrian89@gmail.com>
2024-05-25 22:15:42 +0300

    Day 5 Challenge

---
+++ day-5100-days/fix.py
@@ -0,0 +1,29 @@
+# Fix this code to make it work!
+"""
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs = "cat":
+  print("Meow")
+else:
+  print("Woof")
+  """
+
+catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+   print("Meow")
+else:
+   print("Woof")
+
+# Fix this code to make it work!
+"""
+drink = input("Do you prefer coffee or tea?)
+if drink = "coffee"
+  print("Tea is better.")
+    else:
+  print("Excellent choice.")
+"""
+
+drink = input("Do you prefer coffee or tea?)")
+if drink == "coffee":
+  print("Tea is better.")
+else:
+  print("Excellent choice.")
---
+++ day-5100-days/main.py
@@ -0,0 +1,19 @@
+print("Marvel Movie Character Creator")
+print()
+hangingAround = input("Do you like 'hanging around'?: ")
+if hangingAround == "No":
+  print("Then you're not Spider-man")
+else:
+  print("You're Spider-man")
+print()
+gravellyVoice = input("Do you have a 'gravelly' voice?: ")
+if gravellyVoice == "No":
+  print("Aww, then you're not Korg")
+else:
+  print("You're Korg")
+print()
+marvelous = input("Do you often feel 'Marvelous'?: ")
+if marvelous == "Yes":
+  print("Aha! You're Captain Marvel! Hi!")
+else:
+  print("You're not Captain Marvel")
