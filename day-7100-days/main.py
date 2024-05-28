17d92ee1a8444d5e60101150a0f8cb39210f69ed
stepholo <oloobrian89@gmail.com>
2024-05-28 23:16:04 +0300

    Day 7 Challenge

---
+++ day-7100-days/main.py
@@ -0,0 +1,12 @@
+movie = input("What is your favorite movie? ")
+if movie == "The Matrix":
+  print("Wow, that's a great movie!")
+  character = input("Who is your favorite character? ")
+  if character == "Neo":
+    print("Neo is the best!")
+  else:
+    print("That's a good choice, but Neo is the best.")
+elif movie == "The Dark Knight":
+  print("That's a good movie, but it's not the best.")
+else:
+  print("I haven't seen that movie, but it sounds interesting.")
